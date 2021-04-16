cd4f5f02c08b36eb65bf474d87d5d3a8b9040261
YSW2000 <yang1949@purdue.edu>
2021-04-16 15:46:10 +0800

    Connection

--- PJ05/src/MessageClient.java
+++ PJ05/src/MessageClient.java
@@ -1,14 +1,74 @@
 import Field.User;
 
-public class MessageClient
-{
+import javax.swing.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class MessageClient {
 
     private User user;
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
+        String hostname;
+        String portString;
+        int port;
+        Socket socket = null;
+        BufferedReader bfr = null;
+        PrintWriter pw = null;
+
+        try {
+            hostname = JOptionPane.showInputDialog(null, "Please enter the hostname:",
+                    "Connecting...", JOptionPane.INFORMATION_MESSAGE);
+
+            portString = JOptionPane.showInputDialog(null, "Please enter the port number:",
+                    "Connecting...", JOptionPane.INFORMATION_MESSAGE);
+
+            port = Integer.parseInt(portString);
+
+            socket = new Socket(hostname, port);
+
+            bfr = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            pw = new PrintWriter(socket.getOutputStream());
+
+        } catch (NumberFormatException e) {
+            JOptionPane.showMessageDialog(null, "Invalid input!", "Connecting...",
+                    JOptionPane.ERROR_MESSAGE);
+            return;
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(null, "Connection failed!", "Connecting...",
+                    JOptionPane.ERROR_MESSAGE);
+            return;
+        } finally {
+            try {
+                if (bfr != null) {
+                    bfr.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            if (pw != null) {
+                pw.close();
+            }
+            try {
+                if (socket != null) {
+                    socket.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } //connection established
+
+
+
+
 
         //TODO use calculator client code, establish connection
+
 
         //TODO display main UI, popup sign-in window.
 
