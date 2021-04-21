93e9c7d53e17877511322d5383f296ffc53f4798
Jacky-JQ <82580926+Jacky-JQ@users.noreply.github.com>
2021-04-22 02:25:13 +0800

    Updata of database
    A prototype of reading TXT and put it in map, no direct file way of 'conversation', 'message' and also 'uuid'.
    Still writing about detail, this is the original one, and no debug, next step is to turn the return type from String to Int

--- PJ05/src/Database.java
+++ PJ05/src/Database.java
@@ -1,8 +1,5 @@
 import Field.Storable;
-
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
+import java.io.*;
 import java.util.HashMap;
 import java.util.Set;
 import java.util.UUID;
@@ -17,6 +14,23 @@ public class Database<V extends Storable>
         map = new HashMap<>();
         this.clazz = clazz;
         //TODO read the file and store data in `map`
+        File file = new File(filename);
+        int count = 0;
+        if (file.isFile() && file.exists()) { // 判断文件是否存在
+            InputStreamReader read = new InputStreamReader(new FileInputStream(file), filename);
+            BufferedReader bufferedReader = new BufferedReader(read);
+            String lineTxt = null;
+            while ((lineTxt = bufferedReader.readLine()) != null) {
+                System.out.println("lineTxt=" + lineTxt);
+                if(!"".equals(lineTxt)){
+                    String reds = lineTxt.split("//+")[0];
+                    System.out.println(reds);
+                    map.put(count, reds);
+                    count ++;
+                }
+            }
+        }
+
         ObjectInputStream objectInputStream;
         try
         {
