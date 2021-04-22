cb90a989929ad2dcf03cfd44eb7326052c36ba6e
felix_liu <feliconut@outlook.com>
2021-04-22 15:24:11 +0800

    Add .equals() to fields

--- PJ05/src/Field/Message.java
+++ PJ05/src/Field/Message.java
@@ -1,28 +1,39 @@
 package Field;
 
 import java.util.Date;
+import java.util.Objects;
 import java.util.UUID;
 
-public class Message extends Storable
-{
+public class Message extends Storable {
     public final UUID sender_uuid;
     public final Date time;
     public final String content;
 
 
-    public Message(UUID uuid, UUID sender_uuid, Date time, String content)
-    {
+    public Message(UUID uuid, UUID sender_uuid, Date time, String content) {
         super(uuid);
         this.sender_uuid = sender_uuid;
         this.time = time;
         this.content = content;
     }
 
-    public Message(UUID sender_uuid, Date time, String content)
-    {
-        super(UUID.randomUUID());
+    public Message(UUID sender_uuid, Date time, String content) {
+        super();
         this.sender_uuid = sender_uuid;
         this.time = time;
         this.content = content;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Message)) return false;
+        Message message = (Message) o;
+        return sender_uuid.equals(message.sender_uuid) && time.equals(message.time) && content.equals(message.content);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(sender_uuid, time, content);
     }
 }
