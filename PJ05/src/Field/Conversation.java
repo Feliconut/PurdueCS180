8109c500a606d64aeb8ad99fcd513771bd282a72
felix_liu <feliconut@outlook.com>
2021-04-22 15:24:11 +0800

    Add .equals() to fields

--- PJ05/src/Field/Conversation.java
+++ PJ05/src/Field/Conversation.java
@@ -1,10 +1,10 @@
 package Field;
 
 import java.util.Arrays;
+import java.util.Objects;
 import java.util.UUID;
 
-public class Conversation extends Storable
-{
+public class Conversation extends Storable {
     public String name;
     public UUID[] user_uuids;
     public UUID admin_uuid;
@@ -20,13 +20,22 @@ public class Conversation extends Storable
     }
 
     public Conversation(String name, UUID[] user_uuids, UUID admin_uuid, UUID[] message_uuids) {
-        super(UUID.randomUUID());
-        this.name = name;
-        this.user_uuids = user_uuids;
-        this.admin_uuid = admin_uuid;
-        this.message_uuids = message_uuids;
-        assert Arrays.asList(user_uuids).contains(admin_uuid);
+        this(UUID.randomUUID(), name, user_uuids, admin_uuid, message_uuids);
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Conversation)) return false;
+        Conversation that = (Conversation) o;
+        return name.equals(that.name) && Arrays.equals(user_uuids, that.user_uuids) && Arrays.equals(message_uuids, that.message_uuids);
+    }
 
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(name);
+        result = 31 * result + Arrays.hashCode(user_uuids);
+        result = 31 * result + Arrays.hashCode(message_uuids);
+        return result;
+    }
 }
--- PJ05/src/Field/Credential.java
+++ PJ05/src/Field/Credential.java
@@ -1,16 +1,27 @@
 package Field;
 
 import java.io.Serializable;
+import java.util.Objects;
 
-public class Credential implements Serializable
-{
+public class Credential implements Serializable {
     public final String usrName;
     public final String passwd;
 
-    public Credential(String usrName, String passwd)
-    {
+    public Credential(String usrName, String passwd) {
         this.usrName = usrName;
         this.passwd = passwd;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Credential)) return false;
+        Credential that = (Credential) o;
+        return Objects.equals(usrName, that.usrName) && Objects.equals(passwd, that.passwd);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(usrName, passwd);
+    }
 }
