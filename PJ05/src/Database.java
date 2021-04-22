846c724625d41d190de792022f06bbcbde85fcbd
felix_liu <feliconut@outlook.com>
2021-04-22 14:54:38 +0800

    Functional Database, not tested

--- PJ05/src/Database.java
+++ PJ05/src/Database.java
@@ -1,85 +1,95 @@
 import Field.Storable;
+
 import java.io.*;
 import java.util.HashMap;
 import java.util.Set;
 import java.util.UUID;
 
-public class Database<V extends Storable>
-{
+public class Database<V extends Storable> {
     private final HashMap<UUID, V> map; // This is used as a cache.
     private final Class<V> clazz;
+    private final File file;
 
-    public Database(String filename, Class<V> clazz)
-    {
+    public Database(String filename, Class<V> clazz) {
         map = new HashMap<>();
         this.clazz = clazz;
-        //TODO read the file and store data in `map`
-        File file = new File(filename);
-        int count = 0;
-        if (file.isFile() && file.exists()) { // 判断文件是否存在
-            InputStreamReader read = new InputStreamReader(new FileInputStream(file), filename);
-            BufferedReader bufferedReader = new BufferedReader(read);
-            String lineTxt = null;
-            while ((lineTxt = bufferedReader.readLine()) != null) {
-                System.out.println("lineTxt=" + lineTxt);
-                if(!"".equals(lineTxt)){
-                    String reds = lineTxt.split("//+")[0];
-                    System.out.println(reds);
-                    map.put(count, reds);
-                    count ++;
+
+        //read the file and store data in `map`
+        this.file = new File(filename);
+
+
+        try {
+            if (!file.isFile() && file.exists()) {
+                throw new FileNotFoundException("This is not a file.");
+            }
+            if (!file.getName().endsWith("txt")) {
+                throw new FileNotFoundException("This is not a .txt file.");
+            }
+            // create new file if it does not exist.
+            if (!file.createNewFile()) {
+                // only if the file is not new, we do need to read its data.
+                try (FileInputStream fis = new FileInputStream(file);
+                     ObjectInputStream objectInputStream = new ObjectInputStream(fis)) {
+
+                    while (true) {
+                        try {
+                            // read a new object from the file
+                            Storable storable = (Storable) objectInputStream.readObject();
+                            // put the object into memory
+                            map.put(storable.uuid, clazz.cast(storable));
+                        } catch (EOFException e) {
+                            // we reach end of file. end the loop.
+                            break;
+                        }
+                    }
                 }
             }
-        }
 
-        ObjectInputStream objectInputStream;
-        try
-        {
-            objectInputStream = new ObjectInputStream(new FileInputStream(filename));
-        } catch (IOException e)
-        {
+
+        } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
-            return;
+            System.out.println("Database Initialization Error");
         }
-
-
-        String line = "";
-        try
-        {
-            Storable obj = (Storable) objectInputStream.readObject();
-
-            map.put(obj.uuid, clazz.cast(obj));
-
-        } catch (IOException | ClassNotFoundException e)
-        {
-            e.printStackTrace();
-        }
-
     }
 
-    public void write()
-    {
-        //TODO write the new data into file.
+    public void write() {
+        try {
+            // we refresh the file.
+            file.delete();
+            file.createNewFile();
+
+            try (FileOutputStream fos = new FileOutputStream(file);
+                 ObjectOutputStream objectOutputStream = new ObjectOutputStream(fos)) {
+
+                for (UUID uuid :
+                        map.keySet()) {
+                    Storable storable = map.get(uuid);
+                    objectOutputStream.writeObject(clazz.cast(storable));
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    public V get(UUID uuid)
-    {
+    public V get(UUID uuid) {
         return map.get(uuid);
     }
 
-    public V put(UUID uuid, V value)
-    {
-
+    public V put(UUID uuid, V value) {
         return map.put(uuid, value);
     }
 
-    public boolean containsKey(UUID uuid)
-    {
+    public boolean containsKey(UUID uuid) {
         return map.containsKey(uuid);
     }
 
-    public Set<UUID> uuids()
-    {
+    public Set<UUID> uuids() {
         return map.keySet();
+    }
+
+    public V remove(UUID uuid) {
+        return map.remove(uuid);
     }
 
 
