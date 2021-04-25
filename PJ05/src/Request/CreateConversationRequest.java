2798b468b81d7f44094533e2dc8d229d80315dae
fang296 <fang296@purdue.edu>
2021-04-25 11:53:07 +0800

    Complete the other frameworks except SetAdminRequest and GetEventRequest

--- PJ05/src/Request/CreateConversationRequest.java
+++ PJ05/src/Request/CreateConversationRequest.java
@@ -3,10 +3,10 @@ package Request;
 import java.util.UUID;
 
 public class CreateConversationRequest extends Request {
-    public final UUID user_uuids;
+    public final UUID[] user_uuids;
     public final String name;
 
-    public CreateConversationRequest(UUID user_uuids, String name) {
+    public CreateConversationRequest(UUID[] user_uuids, String name) {
         super();
         this.user_uuids = user_uuids;
         this.name = name;
--- PJ05/src/Request/EditMessageRequest.java
+++ PJ05/src/Request/EditMessageRequest.java
@@ -1,16 +1,19 @@
 package Request;
 
+import java.util.Date;
 import java.util.UUID;
 
 public class EditMessageRequest extends Request {
 
     public final UUID messsage_uuid;
     public final String content;
+    public final Date date;
 
 
-    public EditMessageRequest(UUID messsage_uuid, String content) {
+    public EditMessageRequest(UUID messsage_uuid, String content, Date date) {
         super();
         this.messsage_uuid = messsage_uuid;
         this.content = content;
+        this.date = date;
     }
 }
